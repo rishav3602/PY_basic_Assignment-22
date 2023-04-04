6bb236045e782b060b5e441e183bd7abaa526446
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2023-04-04 15:01:04 +0530

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -9,7 +9,8 @@ print(X)
 
 ANSWER..
 
-The code defines a variable X as 'iNeuron' outside of the function func(). The function func() prints the value of X, which is 'iNeuron' because it is accessing the global variable. When func() is called, it prints 'iNeuron'.
+The code defines a variable X as 'iNeuron' outside of the function func(). The function func() prints the value of X,
+which is 'iNeuron' because it is accessing the global variable. When func() is called, it prints 'iNeuron'.
 
 
 
@@ -26,7 +27,10 @@ X = &#39;NI!&#39;
 
 ANSWER..
 
-The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines a local variable X as 'NI!', which does not affect the global variable. When func() is called, it does not print anything. When print(X) is called outside of the function, it prints 'iNeuron' because the global variable was not changed by the function.
+The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines a
+local variable X as 'NI!', which does not affect the global variable. When func() is called, it does not
+print anything. When print(X) is called outside of the function, it prints 'iNeuron' because the global
+variable was not changed by the function.
 
 
 
@@ -44,7 +48,9 @@ print(X)
 
 ANSWER..
 
-The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines a local variable X as 'NI' and prints it. When func() is called, it prints 'NI'. When print(X) is called outside of the function, it prints 'iNeuron' because the global variable was not changed by the function.
+The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines
+a local variable X as 'NI' and prints it. When func() is called, it prints 'NI'. When print(X) is called
+outside of the function, it prints 'iNeuron' because the global variable was not changed by the function.
 
 -----------------------------------------------------------------------------------------------
 
@@ -60,7 +66,9 @@ X = &#39;NI&#39;
 
 ANSWER..
 
-The code defines a variable X as 'iNeuron' outside of the function func(). The function func() uses the "global" keyword to modify the global variable X to 'NI'. When func() is called, it modifies the global variable X to 'NI'. When print(X) is called outside of the function, it prints 'NI' because the global variable was changed by the function.
+The code defines a variable X as 'iNeuron' outside of the function func(). The function func() uses the "global"
+keyword to modify the global variable X to 'NI'. When func() is called, it modifies the global variable X to
+'NI'. When print(X) is called outside of the function, it prints 'NI' because the global variable was changed by the function.
 
 
 
@@ -80,7 +88,9 @@ nested()
 
 ANSWER..
 
-The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines a local variable X as 'NI' and then defines a nested function nested() that prints the value of X. When func() is called, it calls nested(), which prints 'NI'. The value of X outside of the function is still 'iNeuron'.
+The code defines a variable X as 'iNeuron' outside of the function func(). The function func() defines
+a local variable X as 'NI' and then defines a nested function nested() that prints the value of X.
+When func() is called, it calls nested(), which prints 'NI'. The value of X outside of the function is still 'iNeuron'.
 
 
 
@@ -100,7 +110,9 @@ print(X)
 
 ANSWER..
 
-The code defines a function func() that defines a local variable X as 'NI' and then defines a nested function nested() that uses the "nonlocal" keyword to modify the variable X to 'Spam'. When func() is called, it calls nested(), which modifies the value of X to 'Spam'. When func() is finished, it prints the value of X, which is 'Spam'.
+The code defines a function func() that defines a local variable X as 'NI' and then defines a nested function
+nested() that uses the "nonlocal" keyword to modify the variable X to 'Spam'. When func() is called, it calls
+nested(), which modifies the value of X to 'Spam'. When func() is finished, it prints the value of X, which is 'Spam'.
 
 
 -----------------------------------------------------------------------------------------------
